01f0c95360658b03ecb35ca25bdd978ba3ff2358
ArtemijAE <desnogun2006@gmail.com>
2025-11-27 12:52:04 +0300

    tests

--- tasks/ilin_a_alternations_signs_of_val_vec/tests/functional/main.cpp
+++ tasks/ilin_a_alternations_signs_of_val_vec/tests/functional/main.cpp
@@ -88,7 +88,7 @@ TEST_P(IlinARunFuncTestsProcesses, AlternationsSigns) {
   ExecuteTest(GetParam());
 }
 
-const std::array<TestType, 16> kTestParam = {
+const std::array<TestType, 15> kTestParam = {
     std::make_tuple(10, "alternating"),   std::make_tuple(100, "alternating"),  std::make_tuple(1000, "alternating"),
     std::make_tuple(10, "all_positive"),  std::make_tuple(100, "all_positive"), std::make_tuple(10, "all_negative"),
     std::make_tuple(100, "all_negative"), std::make_tuple(50, "random"),        std::make_tuple(500, "random"),
